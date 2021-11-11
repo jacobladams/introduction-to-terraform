5e8455eb6ab1b31a633077a5f1725567c9b14792
Jacob Adams <jacobladams@gmail.com>
2021-11-10 22:53:52 -0600

    initial commit

---
+++ 1-hello-world/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.84"
+    }
+  }
+  required_version = ">= 1.0.11"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-terraform-demo"
+  location = "CentralUS"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "stterraformdemo314"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
